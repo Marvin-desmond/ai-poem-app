cfa16791766b10d66f8f936c5a03c189ce407190
Marvin-desmond <marvindesmond@gmail.com>
2024-07-24 14:54:10 -0400

    add io and image_picker

--- lib/common.dart
+++ lib/common.dart
@@ -1,4 +1,5 @@
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+export 'dart:io';
 
 export 'package:flutter/rendering.dart';
 export 'package:flutter/material.dart';
@@ -10,6 +11,7 @@ export 'package:provider/provider.dart';
 export 'package:get_it/get_it.dart';
 export 'package:go_router/go_router.dart';
 export 'package:flextras/flextras.dart';
+export 'package:image_picker/image_picker.dart';
 
 export 'package:ai_poem_app/image_logic/image_model.dart';
 export 'package:ai_poem_app/image_logic/image_model_class.dart';
