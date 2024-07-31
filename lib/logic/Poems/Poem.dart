7c8d46bd495d117fdb0d5a891f004c36507078a0
Marvin-desmond <marvindesmond@gmail.com>
2024-07-31 15:18:12 -0400

    implementation of response structures for poems, metadata

--- lib/logic/Poems/Poem.dart
+++ lib/logic/Poems/Poem.dart
@@ -1,5 +1,37 @@
+import 'dart:typed_data';
 
+import 'package:flutter/foundation.dart';
 
+class Metadata {
+    final String id;
+    final String poemId;
+    final String fileId;
+    const Metadata({
+    required this.id,
+    required this.poemId,
+    required this.fileId,
+  });
+    Metadata.fromJson(Map<String, dynamic> json)
+      : id = json['_id'],
+        poemId = json['poem_id'],
+        fileId = json['file_id'];
+}
+
+class MetaStream {
+    final bool streamDone;
+    final bool metaDone;
+    final String buffer;
+  
+    const MetaStream({
+    required this.streamDone,
+    required this.metaDone,
+    required this.buffer,
+  });
+    MetaStream.fromJson(Map<String, dynamic> json)
+      : streamDone = json['stream_done'],
+        metaDone = json['meta_done'],
+        buffer = json['buffer'];
+}
 
 class Poem {
   final String id;
@@ -7,6 +39,11 @@ class Poem {
   final List<String> imaginePrompt;
   final List<DateTime> lastModified;
   final List<String> fileId;
+  Uint8List? buffer;
+
+    Poem(
+      this.id,this.poem,this.imaginePrompt,
+      this.lastModified,this.fileId,this.buffer);
 
   Poem.fromJson(Map<String, dynamic> json)
       : id = json['_id'],
@@ -16,6 +53,54 @@ class Poem {
             .map((dateString) => DateTime.parse(dateString))
             .toList(),
         fileId = List<String>.from(json['file_id']);
+}
+
+// TO-DO
+class BaseResponse<T> {
+  final int status;
+  T data;
+  final String message;
+
+    BaseResponse({
+    required this.status,
+    required this.data,
+    required this.message,
+  });
+}
+// END TO-DO
+
+class PicResponse {
+  final int status;
+  final MetaStream data;
+  final String message;
+
+  const PicResponse({
+    required this.status,
+    required this.data,
+    required this.message,
+  });
+
+  PicResponse.fromJson(Map<String, dynamic> json)
+      : status = json['status'],
+        data = MetaStream.fromJson(json['data']),
+        message = json['message'];
+}
+
+class MetaResponse {
+  final int status;
+  final List<Metadata> data;
+  final String message;
+
+  const MetaResponse({
+    required this.status,
+    required this.data,
+    required this.message,
+  });
+  MetaResponse.fromJson(Map<String, dynamic> json)
+      : status = json['status'],
+        data =
+            (json['data'] as List).map((item) => Metadata.fromJson(item)).toList(),
+        message = json['message'];
 }
 
 class ApiResponse {
@@ -35,3 +120,25 @@ class ApiResponse {
             (json['data'] as List).map((item) => Poem.fromJson(item)).toList(),
         message = json['message'];
 }
+
+
+class CreateData {
+  final bool acknowledged;
+  final String insertedId;
+  CreateData.fromJson(Map<String, dynamic> json)
+    : acknowledged = json['acknowledged'],
+      insertedId = json['insertedId'];
+}
+
+class CreateResponse {
+  final int status;
+  final CreateData? data;
+  final String message;
+
+  CreateResponse.fromJson(Map<String, dynamic> json)
+      : status = json['status'],
+        data = json['data'] != null ? CreateData.fromJson(json['data']) : null,
+        message = json['message'];
+}
+
+
