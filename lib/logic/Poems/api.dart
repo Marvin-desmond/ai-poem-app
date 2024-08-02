8c8e64f3bf1f69fd2b045d1eb709c84c0f9635ee
Marvin-desmond <marvindesmond@gmail.com>
2024-08-02 18:22:00 -0400

    createPoem, creaetePoem, updatePoem implemented

--- lib/logic/Poems/api.dart
+++ lib/logic/Poems/api.dart
@@ -3,13 +3,13 @@ import 'dart:convert';
 import './Poem.dart';
 
 class Api {
-    String poems = "http://10.0.2.2:8080/api/poems";
-    String files = "http://10.0.2.2:8080/api/pics";
+    String basePoemUrl = "http://10.0.2.2:8080/api/poems";
+    String basePicUrl = "http://10.0.2.2:8080/api/pics";
     String imagines = "http://10.0.2.2:8080/api/imagines";
 
     Future<ApiResponse> getPoems() async {
     final response =
-        await http.get(Uri.parse(poems));
+        await http.get(Uri.parse(basePoemUrl));
 
     if (response.statusCode == 200) {
       return ApiResponse.fromJson(
@@ -21,7 +21,7 @@ class Api {
 
   Future<ApiResponse> getPic() async {
     final response =
-        await http.get(Uri.parse(poems));
+        await http.get(Uri.parse(basePoemUrl));
 
 
     if (response.statusCode == 200) {
@@ -35,7 +35,7 @@ class Api {
   Future<CreateData> createPoem(String poem) async {
     final response =
         await http.post(
-          Uri.parse("$poems/create"),
+          Uri.parse("$basePoemUrl/create"),
           headers: <String, String>{
             'Content-Type': 'application/json; charset=UTF-8',
           },
@@ -47,6 +47,59 @@ class Api {
           jsonDecode(response.body) as Map<String, dynamic>);
     if (res.status == 200) {
       return res.data!;
+    } else {
+      throw Exception(res.message);
+    }
+  }
+
+    Future<CreateData> createPic(String poemId, String prompt) async {
+    final response =
+        await http.post(
+          Uri.parse("$basePicUrl/create"),
+          headers: <String, String>{
+            'Content-Type': 'application/json; charset=UTF-8',
+          },
+          body: jsonEncode(<String, String>{
+            'poem_id': poemId,
+            'prompt': prompt
+          }),
+          );
+    CreateResponse res = CreateResponse.fromJson(
+          jsonDecode(response.body) as Map<String, dynamic>);
+    if (res.status == 200) {
+      return res.data!;
+    } else {
+      throw Exception(res.message);
+    }
+  }
+    Future<UpdateData> updatePoem(String id, String newPoem) async {
+    final response =
+        await http.put(
+          Uri.parse("$basePoemUrl/update"),
+          headers: <String, String>{
+            'Content-Type': 'application/json; charset=UTF-8',
+          },
+          body: jsonEncode(<String, String>{
+            'id': id,
+            'new_poem': newPoem,
+          }),
+          );
+    UpdateResponse res = UpdateResponse.fromJson(
+          jsonDecode(response.body) as Map<String, dynamic>);
+    if (res.status == 200) {
+      return res.data!;
+    } else {
+      throw Exception(res.message);
+    }
+  }
+
+    Future<Poem> getPoem(String poemId) async {
+    final response =
+        await http.get(Uri.parse("$basePoemUrl/get/$poemId"));
+    SinglePoemResponse res = SinglePoemResponse.fromJson(
+          jsonDecode(response.body) as Map<String, dynamic>);
+    if (res.status == 200) {
+      return res.data;
     } else {
       throw Exception(res.message);
     }
