a232e4ca1d52d451baff6cad6b66fcf2634927e1
Marvin-desmond <marvindesmond@gmail.com>
2024-08-10 13:50:39 -0400

    remove unused singleton instance

--- lib/main.dart
+++ lib/main.dart
@@ -6,14 +6,10 @@ void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   // Keep native splash screen up until app is finished bootstrapping
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
-
   registerSingletons();
   runApp(
-      MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (context) => ImageModelClass()),
-        ListenableProvider(create: (context) => PoemNotifier()),
-      ],
+    ChangeNotifierProvider(
+      create: (context) => PoemNotifier(),
       child: MaterialApp.router(
         debugShowCheckedModeBanner: false,
         title: 'AI Poem App',
@@ -29,11 +25,8 @@ void main() async {
 void registerSingletons() {
   // Top level app controller
   GetIt.I.registerLazySingleton<AppLogic>(() => AppLogic());
-  // ImageModels
-  GetIt.I.registerLazySingleton<ImageModelClass>(() => ImageModelClass());
   GetIt.I.registerLazySingleton<PoemNotifier>(() => PoemNotifier());
 }
 
 AppLogic get appLogic => GetIt.I.get<AppLogic>();
-ImageModelClass get imageModelClass => GetIt.I.get<ImageModelClass>();
 PoemNotifier get poemNotifier => GetIt.I.get<PoemNotifier>();
